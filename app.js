6f5f2489811ae18ace7e1c869a79023a8663e15d
MIchael Huang <michaelnctu@gmail.com>
2020-07-27 02:13:12 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -2,9 +2,22 @@ const express = require('express')  //使用express
 const app = express()
 const exphbs = require('express-handlebars')  //handlebars
 const port = 3000
+const mongoose = require('mongoose')
 
 const restList = require('./restaurant.json')
 
+mongoose.connect('mongodb://localhost/Restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
